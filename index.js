1adfa5d31b7a9c7623a434dc7b49326c848dbc56
Masha <mariia.tabala@gmail.com>
2023-04-04 12:00:06 -0700

    add user images, create users json file, edit kindacts json file, create a route for users, declare the middleware in index js, write a function to read users and also get users and user by id

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const kindnessRoutes = require("./routes/kindness");
+const usersRoutes = require("./routes/profile");
 const cors = require("cors");
 require('dotenv').config();
 const PORT = process.env.DEV_PORT;
@@ -10,6 +11,7 @@ const PORT = process.env.DEV_PORT;
 app.use(express.json());
 app.use(cors());
 app.use('/kindness', kindnessRoutes);
+app.use('/profile', usersRoutes);
 
 app.listen(PORT, () => {
     console.log("server is running at port" + PORT)
---
+++ routes/profile.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const usersRouter = express.Router();
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+
+//function to read users
+function readUsers() {
+    const usersFile = fs.readFileSync("./data/users.json");
+    const usersData = JSON.parse(usersFile);
+    return usersData;
+}
+
+//get users
+usersRouter.get("/", (req, res) => {
+    const users = readUsers();
+    const usersData = users.map((user) => {
+        return {
+            name: user.name,
+            description: user.description,
+            image: user.image,
+            id: user.id
+        }
+    })
+    res.status(200).json(usersData);
+});
+
+//get selected user by id
+usersRouter.get("/:id", (req, res) => {
+    const users = readUsers();
+    const user = users.find((user) => user.id === req.params.id);
+    res.status(200).json(user);
+})
+
+
+module.exports = usersRouter;
