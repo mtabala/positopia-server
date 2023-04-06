abcbfbb3e49a1527d15f9dfada9954f57f484840
Masha <mariia.tabala@gmail.com>
2023-04-05 19:57:27 -0700

    define a function to write users, create a post request for a new user

--- routes/profile.js
+++ routes/profile.js
@@ -31,18 +31,58 @@ usersRouter.get("/:id", (req, res) => {
     res.status(200).json(user);
 })
 
+// Function to write users
+function writeUsers(data) {
+    const stringifiedData = JSON.stringify(data);
+    fs.writeFileSync("./data/users.json", stringifiedData);
+}
+
+
+//post user
+usersRouter.post("/", (req, res) => {
+    const users = readUsers();
+
+    const newUser = {
+        id: uuidv4(),
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+        description: req.body.description,
+        image: req.body.image,
+        currentActs: [
+            {
+                id: uuidv4()
+            },
+        ],
+    }
+    users.push(newUser);
+    writeUsers(users);
+
+    res.status(201).send();
+})
+
 usersRouter.post('/login', (req, res) => {
     const loginInfo = req.body;
-
-    console.log('loginINfo: ', loginInfo)
     const users = readUsers();
 
     const user = users.find((user) => user.name === loginInfo.user);
     if (!user) res.send('nothing')
 
     res.json(user)
+})
 
-    console.log('user: ', user)
+usersRouter.post('/journal', (req, res) => {
+    const selectedAct = req.body;
+
+    console.log('selectedAct: ', selectedAct)
+    const users = readUsers();
+
+    const selectedActJournal = users.find((user) => user.name === selectedAct.user);
+    if (!selectedActJournal) res.send('nothing')
+
+    res.json(selectedActJournal)
+
+    console.log('user: ', selectedActJournal)
 
 })
 
