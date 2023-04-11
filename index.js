57c776784aa3af5b6dfe2edf5752640392fcfb29
Masha <mariia.tabala@gmail.com>
2023-04-11 10:09:15 -0700

    install multer, create an upload function from multer and storage, create a post route

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const multer = require('multer');
 const app = express();
 
 const kindnessRoutes = require("./routes/kindness");
@@ -7,6 +8,17 @@ const cors = require("cors");
 require('dotenv').config();
 const PORT = process.env.DEV_PORT;
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public')
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+})
+
+const upload = multer({ storage: storage }).single('file');
+
 //middleware
 app.use(express.json());
 app.use(cors());
@@ -14,7 +26,14 @@ app.use('/kindness', kindnessRoutes);
 app.use('/profile', usersRoutes);
 app.use(express.static("public"));
 app.use("/images", express.static("./public/images"));
-
+app.post('/profile/settings', (req, res) => {
+    upload(req, res, (err) => {
+        if (err) {
+            res.sendStatus(500);
+        }
+        res.send(req.file);
+    })
+})
 
 app.listen(PORT, () => {
     console.log("server is running at port" + PORT)
