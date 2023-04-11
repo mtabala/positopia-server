6ddd02bf9b96d0c0e4082d5253b3214933b53603
Masha <mariia.tabala@gmail.com>
2023-04-11 10:11:01 -0700

    fix the route destination for the photos in the multer disk storage

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.DEV_PORT;
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'public')
+        cb(null, 'public/images')
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + '-' + file.originalname)
